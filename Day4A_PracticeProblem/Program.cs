5cb412633a0ec10d5cc928ba66a5d00ddf92afcd
Rohan <rohandute1@gmail.com>
2023-09-27 21:28:21 +0530

    [rohan]initial commit

---
+++ Day4A_PracticeProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4A_PracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day 4A Practice Problems.");
+            Console.ReadLine();
+        }
+    }
+}
