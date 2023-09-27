4228729b39e5d1307404f27df76e4a65bcb9a61d
Rohan <rohandute1@gmail.com>
2023-09-27 22:09:12 +0530

    [rohan]nlog implemented succesfully

---
+++ Day4A_PracticeProblem/Addition.cs
@@ -0,0 +1,28 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4A_PracticeProblem
+{
+    class Addition
+    {
+        public static Logger log = LogManager.GetCurrentClassLogger();
+        public void Sum(int x, int y)
+        {
+            if (x <= 0 || y <= 0)
+            {
+                log.Error("Please enter positive intergers only.");
+                Console.WriteLine("Please enter positive intergers only.");
+            }
+            else
+            {
+                int add = x + y;
+                Console.WriteLine("Addition of two positive integers: " + add);
+                log.Info("Addition of two positive integers: " + add);
+            }
+        }
+    }
+}
--- Day4A_PracticeProblem/Program.cs
+++ Day4A_PracticeProblem/Program.cs
@@ -11,6 +11,38 @@ namespace Day4A_PracticeProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Day 4A Practice Problems.");
+            bool continueExecution = true;
+
+            while (continueExecution) 
+            {
+                Console.WriteLine("Please select any one of the following program which you wants to perform");
+                Console.WriteLine("1.Nlog problem");
+                
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch(option)
+                {
+                    case 1:
+                        Console.WriteLine("Please enetr 1st number:");
+                        int x = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Please enetr 2nd number:");
+                        int y = Convert.ToInt32(Console.ReadLine());
+                        Addition addition = new Addition();
+                        addition.Sum(x, y);
+                        break;
+
+                    default:
+                        Console.WriteLine("Please choose correct option");
+                        break;
+                }
+                Console.WriteLine("Do you want to continue (yes or no)");
+                string userInput = Console.ReadLine().ToLower();
+                if (userInput != "yes")
+                {
+                    continueExecution = false;
+                } 
+            }
+            
             Console.ReadLine();
         }
     }
